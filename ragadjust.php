0d51600f99247cc25ff69a87a615f339a7d0881e
X-Raym <extremraym@yahoo.fr>
2014-06-08 17:19:42 +0200

    Update ragadjust.php

--- ragadjust.php
+++ ragadjust.php
@@ -1,12 +1,12 @@
 <?php
 /*
-Plugin Name: WP ragadjust
-Plugin URI: http://jameskoster.co.uk/tag/wp-ragadjust/
+Plugin Name: WP ragadjust - French
+Plugin URI: https://github.com/X-Raym/wp-ragadjust/
 Version: 1.0.0
 Description: Includes ragadjust.js to fix common typographical rag violations.
-Author: jameskoster
-Tested up to: 3.8
-Author URI: http://jameskoster.co.uk
+Author: X-Raym
+Tested up to: 3.9
+Author URL: http://extremraym.com
 
 	License: GNU General Public License v3.0
 	License URI: http://www.gnu.org/licenses/gpl-3.0.html
